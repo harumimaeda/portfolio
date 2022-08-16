78080451ef4126b9d1dc0fb3ebdea9607e18372e
Harumi Maeda <harumiumaeda@gmail.com>
2022-08-16 18:44:22 -0300

    Stop fixing header - for today lol

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,84 +25,90 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
+const menuItemStyle = 'text-base font-medium text-neutral-600 hover:text-gray-900';
+
 export default function Header() {
   return (
     <Popover className="relative bg-white">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6">
-        <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
-          <div className="flex justify-start lg:w-0 lg:flex-1">
-            <a href="/">
-              <p className="text-base font-medium text-gray-500 hover:text-gray-900">harumi<b>maeda</b></p>
-            </a>
-          </div>
-          <div className="-mr-2 -my-2 md:hidden">
-            <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-              <span className="sr-only">abrir menu</span>
-              <MenuIcon className="h-6 w-6" aria-hidden="true" />
-            </Popover.Button>
-          </div>
-          <Popover.Group as="nav" className="hidden md:flex space-x-10">
-            <a href="/about" className="text-base font-medium text-gray-500 hover:text-gray-900">
-              sobre
-            </a>
-            <Popover className="relative">
-              {({ open }) => (
-                <>
-                  <Popover.Button
-                    className={classNames(
-                      open ? 'text-gray-900' : 'text-gray-500',
-                      'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-                    )}
-                  >
-                    <span>portfolio</span>
-                    <ChevronDownIcon
+      <div className="border-b-2 border-gray-100">
+        {/* fix: 1. alterar o max-w */}
+        <div className="max-w-7xl mx-auto px-4 sm:px-6">
+          <div className="flex justify-between items-center py-6 md:justify-start md:space-x-10">
+            <div className="flex justify-start lg:w-0 lg:flex-1">
+              <a href="/">
+                <p className="text-xl font-medium text-neutral-600 hover:text-gray-900">harumi<b>maeda</b></p>
+              </a>
+            </div>
+            <div className="-mr-2 -my-2 md:hidden">
+              {/* fix: 2. alterar as cores // teal-400? */}
+              <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-neutral-600 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-teal-500">
+                <span className="sr-only">abrir menu</span>
+                <MenuIcon className="h-6 w-6" aria-hidden="true" />
+              </Popover.Button>
+            </div>
+            <Popover.Group as="nav" className="hidden md:flex space-x-10">
+              <a href="/about" className={ menuItemStyle }>
+                sobre
+              </a>
+              <Popover className="relative">
+                {({ open }) => (
+                  <>
+                    <Popover.Button
                       className={classNames(
-                        open ? 'text-gray-600' : 'text-gray-400',
-                        'ml-2 h-5 w-5 group-hover:text-gray-500'
+                        open ? 'text-gray-900' : 'text-neutral-600',
+                        'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500'
                       )}
-                      aria-hidden="true"
-                    />
-                  </Popover.Button>
+                    >
+                      <span>portfolio</span>
+                      <ChevronDownIcon
+                        className={classNames(
+                          open ? 'text-gray-600' : 'text-gray-400',
+                          'ml-2 h-5 w-5 group-hover:text-neutral-600'
+                        )}
+                        aria-hidden="true"
+                      />
+                    </Popover.Button>
 
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-200"
-                    enterFrom="opacity-0 translate-y-1"
-                    enterTo="opacity-100 translate-y-0"
-                    leave="transition ease-in duration-150"
-                    leaveFrom="opacity-100 translate-y-0"
-                    leaveTo="opacity-0 translate-y-1"
-                  >
-                    <Popover.Panel className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
-                      <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                        <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                          {portfolio.map((item) => (
-                            <a
-                              key={item.name}
-                              href={item.href}
-                              className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
-                            >
-                              <item.icon className="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
-                              <div className="ml-4">
-                                <p className="text-base font-medium text-gray-900">{item.name}</p>
-                              </div>
-                            </a>
-                          ))}
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-200"
+                      enterFrom="opacity-0 translate-y-1"
+                      enterTo="opacity-100 translate-y-0"
+                      leave="transition ease-in duration-150"
+                      leaveFrom="opacity-100 translate-y-0"
+                      leaveTo="opacity-0 translate-y-1"
+                    >
+                      <Popover.Panel className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-xs sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
+                        <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                          <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                            {portfolio.map((item) => (
+                              <a
+                                key={item.name}
+                                href={item.href}
+                                className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
+                              >
+                                <item.icon className="flex-shrink-0 h-6 w-6 text-teal-600" aria-hidden="true" />
+                                <div className="ml-4">
+                                  <p className="text-base font-medium text-gray-900">{item.name}</p>
+                                </div>
+                              </a>
+                            ))}
+                          </div>
                         </div>
-                      </div>
-                    </Popover.Panel>
-                  </Transition>
-                </>
-              )}
-            </Popover>
-            <a href="/contact" className="text-base font-medium text-gray-500 hover:text-gray-900">
-              contato
-            </a>
-
-          </Popover.Group>
+                      </Popover.Panel>
+                    </Transition>
+                  </>
+                )}
+              </Popover>
+              <a href="/contact" className={ menuItemStyle }>
+                contato
+              </a>
+            </Popover.Group>
+          </div>
         </div>
       </div>
 
+      {/* Início menu mobile */}
       <Transition
         as={Fragment}
         enter="duration-200 ease-out"
@@ -118,11 +124,11 @@ export default function Header() {
               <div className="flex items-center justify-between">
                 <div>
                 <a href="/">
-                <p className="text-base font-medium text-gray-500 hover:text-gray-900">harumi<b>maeda</b></p>
+                <p className="text-base font-medium text-neutral-600 hover:text-gray-900">harumi<b>maeda</b></p>
                 </a>
                 </div>
                 <div className="-mr-2">
-                  <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-neutral-600 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-teal-500">
                     <span className="sr-only">fechar menu</span>
                     <XIcon className="h-6 w-6" aria-hidden="true" />
                   </Popover.Button>
@@ -136,7 +142,7 @@ export default function Header() {
                       href={item.href}
                       className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
                     >
-                      <item.icon className="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
+                      <item.icon className="flex-shrink-0 h-6 w-6 text-teal-600" aria-hidden="true" />
                       <span className="ml-3 text-base font-medium text-gray-900">{item.name}</span>
                     </a>
                   ))}
@@ -145,18 +151,18 @@ export default function Header() {
             </div>
             <div className="py-6 px-5 space-y-6">
               <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-                <a href="/about" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                <a href="/about" className={ menuItemStyle }>
                   sobre
                 </a>
 
-                <a href="/teste" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                <a href="/teste" className={ menuItemStyle }>
                   deletar?
                 </a>
               </div>
               <div>
                 <a
                   href="/contact"
-                  className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                  className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-teal-600 hover:bg-teal-700"
                 >
                   entre em contato :)
                 </a>
